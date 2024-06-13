42b06d2c439f5e324bb1b939d27d14a151762874
Mateus Ramos <pereira.mateusramos@outlook.com>
2024-06-13 11:06:39 -0300

    Validação campos do fomulário

--- js/form.js
+++ js/form.js
@@ -4,18 +4,24 @@ botaoAdicionar.addEventListener("click", function (event) {
     event.preventDefault();
 
     var form = document.querySelector("#form-adiciona");
-    
+
     //Extraindo informações do paciente do form
     var paciente = obtemPacienteDoFormulario(form);
 
-    //Cria a tr e a td do paciente
-    var pacienteTr = montaTr(paciente);
+    var erros = validaPaciente(paciente);
+
+    if (erros.length > 0) {
+        exibeMensagensDeErro(erros);
+
+        return;
+    }
+
+    adicionaPacienteNaTabela(paciente);
 
     form.reset();
 
-    var tabela = document.querySelector("#tabela-pacientes");
-
-    tabela.appendChild(pacienteTr);
+    var mensagensErro = document.querySelector("#msg-erro");
+    mensagensErro.innerHTML = "";
 })
 
 function obtemPacienteDoFormulario(form) {
@@ -47,4 +53,60 @@ function montaTd(dado, classe) {
     td.classList.add(classe);
 
     return td;
+}
+
+function validaPaciente(paciente) {
+    var erros = [];
+
+    if (paciente.nome == "" || paciente.nome == null || paciente.nome.length == 0) {
+        erros.push("Nome não pode ser vazio!");
+    }
+    if (paciente.peso == "" || paciente.peso == null) {
+        erros.push("Peso não pode ser vazio!");
+    }
+    if (paciente.altura == "" || paciente.altura == null) {
+        erros.push("Altura não pode ser vazio!");
+    }
+    if (paciente.gordura == "" || paciente.gordura == null) {
+        erros.push("Gordura não pode ser vazio!");
+    }
+    if (!validaAltura) {
+        erros.push("Valor de altura inválido!");
+    }
+    if (!validaPeso) {
+        erros.push("Valor de peso inválido!");
+    }
+    return erros;
+}
+
+function validaPeso(peso) {
+    if (peso > 0 && peso <= 600) {
+        return true;
+    }
+    return false;
+}
+
+function validaAltura(altura) {
+    if (altura > 0 && altura <= 3.00) {
+        return true;
+    }
+    return false;
+}
+
+function exibeMensagensDeErro(erros) {
+    var msgErro = document.querySelector("#msg-erro");
+
+    msgErro.innerHTML = "";
+
+    erros.forEach(erro => {
+        var li = document.createElement("li");
+        li.textContent = erro;
+        msgErro.appendChild(li);
+    });
+}
+
+function adicionaPacienteNaTabela(paciente) {
+    var pacienteTr = montaTr(paciente);
+    var tabela = document.querySelector("#tabela-pacientes");
+    tabela.appendChild(pacienteTr);
 }
